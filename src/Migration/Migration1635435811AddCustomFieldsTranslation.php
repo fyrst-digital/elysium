3930eeafd722f345d0856b08850dd7273bed2058
Robert <robsenq@gmail.com>
2021-10-28 18:00:42 +0200

    move custom_fields migration to entity translation

--- src/Migration/Migration1635435811AddCustomFieldsTranslation.php
+++ src/Migration/Migration1635435811AddCustomFieldsTranslation.php
@@ -5,17 +5,17 @@ namespace Blur\BlurElysiumSlider\Migration;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1635429745AddCustomFields extends MigrationStep
+class Migration1635435811AddCustomFieldsTranslation extends MigrationStep
 {
     public function getCreationTimestamp(): int
     {
-        return 1635429745;
+        return 1635435811;
     }
 
     public function update(Connection $connection): void
     {
         $sql = <<<SQL
-            ALTER TABLE `blur_elysium_slides`
+            ALTER TABLE `blur_elysium_slides_translation`
             ADD COLUMN `custom_fields` json DEFAULT NULL
         SQL;
 
