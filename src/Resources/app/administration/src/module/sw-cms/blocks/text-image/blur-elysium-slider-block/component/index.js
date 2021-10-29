d1c588e8348b4aa0b0a633246d4739a0a3b93e10
Robert <robsenq@gmail.com>
2021-10-29 15:56:31 +0200

    remove empty SCSS files

--- src/Resources/app/administration/src/module/sw-cms/blocks/text-image/blur-elysium-slider-block/component/index.js
+++ src/Resources/app/administration/src/module/sw-cms/blocks/text-image/blur-elysium-slider-block/component/index.js
@@ -1,5 +1,4 @@
 import template from './sw-cms-block-blur-elysium-slider-block.twig';
-import './sw-cms-block-blur-elysium-slider-block.scss';
 
 Shopware.Component.register( 'sw-cms-block-blur-elysium-slider-block', {
     template
--- src/Resources/app/administration/src/module/sw-cms/blocks/text-image/blur-elysium-slider-block/preview/index.js
+++ src/Resources/app/administration/src/module/sw-cms/blocks/text-image/blur-elysium-slider-block/preview/index.js
@@ -1,5 +1,4 @@
 import template from './sw-cms-preview-blur-elysium-slider-block.twig';
-import './sw-cms-preview-blur-elysium-slider-block.scss';
 
 Shopware.Component.register( 'sw-cms-preview-blur-elysium-slider-block', {
     template
