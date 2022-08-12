aebffbe338f5a03a39054de1ddc5cc6371e4fd6a
Robert <robsenq@gmail.com>
2022-08-12 12:37:14 +0200

    add delete logic

--- src/BlurElysiumSlider.php
+++ src/BlurElysiumSlider.php
@@ -23,16 +23,18 @@ class BlurElysiumSlider extends Plugin
     private const MEDIA_THUMBNAIL_SIZES = [
         [
             'width' => 500,
+            'height' => 1000
         ], [
             'width' => 700,
+            'height' => 1400
         ], [
             'width' => 1000,
+            'height' => 1800
         ], [
             'width' => 2000,
+            'height' => 2200,
         ]
     ];
-
-    
 
     public function postInstall(InstallContext $installContext): void
     {
@@ -56,7 +58,11 @@ class BlurElysiumSlider extends Plugin
         $migrationCollection = $uninstallContext->getMigrationCollection(); // get migration collection
 
         if ( $uninstallContext->keepUserData() === false ) {
-            $migrationCollection->migrateDestructiveInPlace( 1624100471 ); // call updateDestructive
+            // call updateDestructive and remove entity from database
+            $migrationCollection->migrateDestructiveInPlace( 1624100471 ); 
+
+            // remove media folder and according default folder
+            $this->removeMediaFolders( $uninstallContext->getContext() );
         } 
     }
 
@@ -99,10 +105,87 @@ class BlurElysiumSlider extends Plugin
                 'id' => $mediaFolderId,
                 'name' => self::MEDIA_FOLDER_NAME,
                 'useParentConfiguration' => false,
-                'configuration' => [], // @TODO set proper folder configuration. i.e. thumbnail sizes
+                'configuration' => [
+                    'mediaThumbnailSizes' => self::MEDIA_THUMBNAIL_SIZES
+                ], // @TODO set proper folder configuration. i.e. thumbnail sizes
                 'defaultFolderId' => $defaultFolderId
             ]
         ], $context);
+    }
+
+    public function removeMediaFolders( $context )
+    {
+        $mediaDefaultFolderId = null;
+        $mediaFolderId = null;
+        $mediaConfigurationId = null;
+        $mediaThumbnailSizes = null;
+
+        $this->setMediaDefaultFolderRepository( $this->container->get('media_default_folder.repository') );
+        $this->setMediaFolderRepository( $this->container->get('media_folder.repository') );
+
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('entity', 'blur_elysium_slides'));
+        $criteria->addAssociation('media_folder');
+        $criteria->setLimit(1);
+
+        // get associated mediaDefaultFolderId, mediaFolderId, mediaConfigurationId
+        $mediaFolderElysiumSlides = $this->getMediaDefaultFolderRepository()->search($criteria, $context)->first();
+
+
+        if ($mediaFolderElysiumSlides === null) {
+            return;
+        }
+
+        if ($mediaFolderElysiumSlides->id) {
+            $mediaDefaultFolderId = $mediaFolderElysiumSlides->id;
+        }
+
+        // existence check
+        if ( $mediaFolderElysiumSlides->folder !== null ) {
+            $mediaFolderId = $mediaFolderElysiumSlides->folder->id;
+            $mediaConfigurationId = $mediaFolderElysiumSlides->folder->configurationId;
+        }
+
+        $criteriaConfiguration = new Criteria();
+        $criteriaConfiguration->addFilter(new EqualsFilter('id', $mediaConfigurationId));
+        $criteriaConfiguration->addAssociation('media_folder_configuration_media_thumbnail_size');
+        $criteriaConfiguration->setLimit(1);
+
+        #dd($this->container->get('media_folder_configuration.repository')->search($criteriaConfiguration, $context)->first()->mediaThumbnailSizes);
+
+        if ( $mediaThumbnailSizes !== null ) {
+            // delete thumbnail sizes related to media config
+        }
+
+        #dd($mediaFolderId, $mediaDefaultFolderId);
+
+        
+        if ( $mediaFolderId !== null ) {
+            // delete media folder entry
+            $this->getMediaFolderRepository()->delete([
+                [
+                    'id' => $mediaFolderId,
+                ]
+            ], $context);
+        }
+
+        if ( $mediaDefaultFolderId !== null ) {
+            // delete media default folder entry
+            $this->getMediaDefaultFolderRepository()->delete([
+                [ 
+                    'id' => $mediaDefaultFolderId
+                ]
+            ], $context); 
+        }
+
+        if ( $mediaConfigurationId ) {
+            // delete media folder configuration entry
+            $this->container->get('media_folder_configuration.repository')->delete([
+                [ 
+                    'id' => $mediaConfigurationId
+                ]
+            ], $context);
+        }
     }
 
     /**
