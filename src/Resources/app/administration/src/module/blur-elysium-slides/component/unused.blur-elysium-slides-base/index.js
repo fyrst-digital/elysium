0b44b96ad17513d90655ae86b57318eab3497b5d
Robert <robsenq@gmail.com>
2022-08-15 11:34:24 +0200

    fix cancel button behavior

--- src/Resources/app/administration/src/module/blur-elysium-slides/component/unused.blur-elysium-slides-base/index.js
+++
@@ -1,8 +0,0 @@
-import template from './blur-elysium-slides-base.html.twig';
-// import './blur-elysium-slides-base.scss';
-
-const { Component, Context, Utils, Mixin } = Shopware;
-
-Component.register( 'blur-elysium-slides-base', {
-    template
-});
