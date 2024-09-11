bbca9f8a3f4010c4c5a476d49c1d845ac6e0dfc3
jinroh <robsenq@gmail.com>
2024-09-11 23:06:13 +0200

    fix(#113): column wrap option toggle

---
+++ src/Resources/public/static/js/11dc5c1bf46246c3492a.js
@@ -0,0 +1,1 @@
+"use strict";(window["webpackJsonpPluginblur-elysium-slider"]=window["webpackJsonpPluginblur-elysium-slider"]||[]).push([[2810],{2810:function(e,i,n){n.r(i),n.d(i,{default:function(){return r}});let{Component:l}=Shopware;var r=l.wrapComponentConfig({template:"<div>\n    <blur-elysium-slides-form-general />\n    <blur-elysium-slides-form-linking />\n</div>"})}}]);
