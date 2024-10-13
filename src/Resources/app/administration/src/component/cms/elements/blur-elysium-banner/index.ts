703f9ffe2fe8b7089041523de4734feecd48a0b7
Robert <robsenq@gmail.com>
2024-10-13 11:15:43 +0200

    fix(#123): split cms banner settings to typed object

--- src/Resources/app/administration/src/component/cms/elements/blur-elysium-banner/index.ts
+++ src/Resources/app/administration/src/component/cms/elements/blur-elysium-banner/index.ts
@@ -1,3 +1,5 @@
+import defaultBannerSettings from 'blurElysium/component/cms/elements/blur-elysium-banner/settings'
+
 Shopware.Component.register(
     'cms-el-blur-elysium-banner', 
     () => import('blurElysium/component/cms/elements/blur-elysium-banner/component')
@@ -19,50 +21,5 @@ Shopware.Service('cmsService').registerCmsElement({
     component: 'cms-el-blur-elysium-banner',
     configComponent: 'cms-el-blur-elysium-banner-config',
     previewComponent: 'cms-el-blur-elysium-banner-preview',
-    defaultConfig: {
-        elysiumSlide: { /** @type object */
-            source: 'static', /** @type 'static' | 'source' */
-            value: '' /** @type string */
-        },
-        lazyLoading: { 
-            source: 'static',
-            value: true
-        },
-        /** @type object */
-        viewports: {
-            /** @type 'static' | 'source' */
-            source: 'static',
-            /** @type object<StandardDevices> */
-            value: {
-                /** @type object<DeviceSettings> */
-                mobile: {
-                    aspectRatio: {
-                        width: 1,
-                        height: 1,
-                        auto: false
-                    },
-                    /** @type null | number */
-                    maxHeight: null
-                },
-                tablet: {
-                    aspectRatio: {
-                        width: 4,
-                        height: 3,
-                        auto: false
-                    },
-                    /** @type null | number */
-                    maxHeight: null
-                },
-                desktop: {
-                    aspectRatio: {
-                        width: 16,
-                        height: 9,
-                        auto: false
-                    },
-                    /** @type null | number */
-                    maxHeight: null
-                }
-            }
-        }
-    }
+    defaultConfig: defaultBannerSettings
 })
