81afd9b20bdd014c456c9ef948edd383a95e60fe
Robert <robsenq@gmail.com>
2024-11-07 11:53:35 +0100

    hide section notifications behind feature flag

--- src/Resources/app/administration/src/extension/sw-cms-sidebar/index.ts
+++ src/Resources/app/administration/src/extension/sw-cms-sidebar/index.ts
@@ -1,12 +1,16 @@
 import template from './template.html.twig'
 
 // eslint-disable-next-line no-undef
-const { Component } = Shopware
+const { Component, Feature } = Shopware
 
 Component.override('sw-cms-sidebar', {
     template,
 
     computed: {
+        isSectionFeatureActive () {
+            return Feature.isActive('BLUR_ELYSIUM_CMS_SECTION') ?? false
+        },
+
         elysiumSectionSettingsTitle() {
             if (this.selectedBlock !== null) {
                 return this.$tc('blurElysiumSection.sidebar.blockSettingsTitle')
