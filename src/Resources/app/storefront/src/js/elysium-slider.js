f4922cd25ac49522b755dd70f4fb762b757fb502
ROBJkE <robsenq@gmail.com>
2025-07-13 14:25:25 +0200

    wip: adjust slider navigation styling and make it work with multiple slider elements

--- src/Resources/app/storefront/src/js/elysium-slider.js
+++ src/Resources/app/storefront/src/js/elysium-slider.js
@@ -19,14 +19,11 @@ export default class ElysiumSlider extends PluginBaseClass {
     init() {
         const inlineOptions = typeof this.el.dataset.swiperOptions === 'string' ? JSON.parse(this.el.dataset.swiperOptions) : {}
         const swiperElement = this.el.querySelector(this.options.swiperSelector)
+
         this.swiper = new Swiper(swiperElement, deepmerge({
             a11y: true,
-            observer: true,
             modules: [Autoplay, Navigation, Pagination],
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true
-            },
+            pagination: false,
         }, inlineOptions))
     }
 }
