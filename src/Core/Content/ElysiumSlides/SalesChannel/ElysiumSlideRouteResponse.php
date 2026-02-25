2072ca003e88663c09937181dafd98fab861b919
jinroh <robsenq@gmail.com>
2026-02-25 14:15:13 +0700

    fix: add union type to ElysiumSlideRouteResponse

--- src/Core/Content/ElysiumSlides/SalesChannel/ElysiumSlideRouteResponse.php
+++ src/Core/Content/ElysiumSlides/SalesChannel/ElysiumSlideRouteResponse.php
@@ -6,16 +6,17 @@ namespace Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\SalesChannel;
 
 use Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\ElysiumSlidesCollection;
 use Shopware\Core\System\SalesChannel\StoreApiResponse;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 
 class ElysiumSlideRouteResponse extends StoreApiResponse
 {
     public function __construct(
-        private readonly ElysiumSlidesCollection $slides,
+        private readonly ElysiumSlidesCollection|EntityCollection $slides,
     ) {
         parent::__construct($slides);
     }
 
-    public function getSlides(): ElysiumSlidesCollection
+    public function getSlides(): ElysiumSlidesCollection|EntityCollection
     {
         return $this->slides;
     }
