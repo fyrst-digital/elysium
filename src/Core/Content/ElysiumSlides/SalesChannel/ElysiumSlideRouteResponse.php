29c5c30b17e33625b55b53a0ad7cd178b9c4f690
jinroh <robsenq@gmail.com>
2026-02-25 14:19:38 +0700

    fix: remove constructor in ElysiumSlideRouteResponse

--- src/Core/Content/ElysiumSlides/SalesChannel/ElysiumSlideRouteResponse.php
+++ src/Core/Content/ElysiumSlides/SalesChannel/ElysiumSlideRouteResponse.php
@@ -6,18 +6,11 @@ namespace Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\SalesChannel;
 
 use Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\ElysiumSlidesCollection;
 use Shopware\Core\System\SalesChannel\StoreApiResponse;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 
 class ElysiumSlideRouteResponse extends StoreApiResponse
 {
-    public function __construct(
-        private readonly ElysiumSlidesCollection|EntityCollection $slides,
-    ) {
-        parent::__construct($slides);
-    }
-
-    public function getSlides(): ElysiumSlidesCollection|EntityCollection
+    public function getSlides(): ElysiumSlidesCollection
     {
-        return $this->slides;
+        return $this->object;
     }
 }
