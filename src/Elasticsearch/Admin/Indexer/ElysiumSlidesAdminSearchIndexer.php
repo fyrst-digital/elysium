7b6d40ff6586b939dd122e7ec9974c5f01f37ce5
jinroh <robsenq@gmail.com>
2026-03-15 12:48:23 +0700

    fix indexer class

--- src/Elasticsearch/Admin/Indexer/ElysiumSlidesAdminSearchIndexer.php
+++ src/Elasticsearch/Admin/Indexer/ElysiumSlidesAdminSearchIndexer.php
@@ -4,99 +4,89 @@ declare(strict_types=1);
 
 namespace Blur\BlurElysiumSlider\Elasticsearch\Admin\Indexer;
 
+use Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\ElysiumSlidesCollection;
 use Blur\BlurElysiumSlider\Core\Content\ElysiumSlides\ElysiumSlidesDefinition;
+use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\IterableQuery;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\IteratorFactory;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Elasticsearch\Admin\Indexer\AbstractAdminIndexer;
 
 final class ElysiumSlidesAdminSearchIndexer extends AbstractAdminIndexer
 {
+    /**
+     * @internal
+     *
+     * @param EntityRepository<ElysiumSlidesCollection> $repository
+     */
     public function __construct(
         private readonly Connection $connection,
         private readonly IteratorFactory $iteratorFactory,
+        private readonly EntityRepository $repository,
+        private readonly int $indexingBatchSize
     ) {}
+
+    public function getDecorated(): AbstractAdminIndexer
+    {
+        throw new DecorationPatternException(self::class);
+    }
+
+    public function getEntity(): string
+    {
+        return ElysiumSlidesDefinition::ENTITY_NAME;
+    }
 
     public function getName(): string
     {
         return ElysiumSlidesDefinition::ENTITY_NAME;
     }
 
-    public function getEntity(): string
+    public function getIterator(): IterableQuery
     {
-        return ElysiumSlidesDefinition::class;
+        return $this->iteratorFactory->createIterator($this->getEntity(), null, $this->indexingBatchSize);
     }
 
-    public function getMapping(array $mapping): array
+    public function fetch(array $ids): array
     {
-        $mapping['properties'] = [
-            'id' => self::KEYWORD_FIELD,
-            'name' => self::SEARCH_FIELD,
-            'title' => self::SEARCH_FIELD,
-            'description' => self::SEARCH_FIELD,
-            'url' => self::SEARCH_FIELD,
-            'buttonLabel' => self::SEARCH_FIELD,
-            'createdAt' => self::DATETIME_FIELD,
-            'updatedAt' => self::DATETIME_FIELD,
-        ];
+        $data = $this->connection->fetchAllAssociative(
+            'SELECT LOWER(HEX(elysium_slides.id)) as id,
+                    GROUP_CONCAT(DISTINCT translation.name SEPARATOR " ") as name,
+                    GROUP_CONCAT(DISTINCT translation.title SEPARATOR " ") as title
+             FROM blur_elysium_slides elysium_slides
+                INNER JOIN blur_elysium_slides_translation translation
+                    ON elysium_slides.id = translation.blur_elysium_slides_id
+             WHERE elysium_slides.id IN (:ids)
+             GROUP BY elysium_slides.id',
+            [
+                'ids' => Uuid::fromHexToBytesList($ids),
+            ],
+            [
+                'ids' => ArrayParameterType::BINARY,
+            ]
+        );
 
-        return $mapping;
-    }
-
-    public function getIterator(): iterable
-    {
-        $iterator = $this->iteratorFactory->createIterator($this->getEntity());
-
-        foreach ($iterator as $id) {
-            yield ['id' => Uuid::fromBytesToHex($id['id'])];
+        $mapped = [];
+        foreach ($data as $row) {
+            $id = (string) $row['id'];
+            $text = \implode(' ', array_filter($row));
+            $mapped[$id] = ['id' => $id, 'text' => \strtolower($text)];
         }
+
+        return $mapped;
     }
 
-    public function fetch(array $ids, Context $context, Criteria $criteria): array
+    public function globalData(array $result, Context $context): array
     {
-        $bytes = array_map(fn(string $id) => Uuid::fromHexToBytes($id), $ids);
+        $ids = array_column($result['hits'], 'id');
 
-        $query = $this->connection->createQueryBuilder();
-        $query->select([
-            'LOWER(HEX(elysium_slides.id)) AS id',
-            'COALESCE(translation.name, translation_fallback.name) AS name',
-            'COALESCE(translation.title, translation_fallback.title) AS title',
-            'COALESCE(translation.description, translation_fallback.description) AS description',
-            'COALESCE(translation.url, translation_fallback.url) AS url',
-            'COALESCE(translation.button_label, translation_fallback.button_label) AS buttonLabel',
-            'elysium_slides.created_at AS createdAt',
-            'elysium_slides.updated_at AS updatedAt',
-        ]);
-        $query->from('blur_elysium_slides', 'elysium_slides');
-        $query->leftJoin(
-            'elysium_slides',
-            'blur_elysium_slides_translation',
-            'translation',
-            'elysium_slides.id = translation.blur_elysium_slides_id 
-            AND translation.language_id = :languageId'
-        );
-        $query->leftJoin(
-            'elysium_slides',
-            'blur_elysium_slides_translation',
-            'translation_fallback',
-            'elysium_slides.id = translation_fallback.blur_elysium_slides_id 
-            AND translation_fallback.language_id = :fallbackLanguageId'
-        );
-        $query->andWhere('elysium_slides.id IN (:ids)');
-        $query->setParameter('ids', $bytes, Connection::PARAM_STR_ARRAY);
-        $query->setParameter('languageId', Uuid::fromHexToBytes($context->getLanguageId()));
-        $query->setParameter('fallbackLanguageId', Uuid::fromHexToBytes($context->getFallbackLanguageId()));
-
-        return $query->executeQuery()->fetchAllAssociativeIndexed();
-    }
-
-    public function globalData(Context $context, Criteria $criteria): array
-    {
         return [
-            'total' => (int) $this->connection->fetchOne('SELECT COUNT(*) FROM blur_elysium_slides'),
+            'total' => (int) $result['total'],
+            'data' => $this->repository->search(new Criteria($ids), $context)->getEntities(),
         ];
     }
 }
