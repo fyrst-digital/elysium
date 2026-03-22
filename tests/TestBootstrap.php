8fddeba709b3d61c0c4d02b2ad0f9328bb70bff5
jinroh <robsenq@gmail.com>
2026-03-22 18:44:48 +0700

    fix classes

--- tests/TestBootstrap.php
+++ tests/TestBootstrap.php
@@ -4,8 +4,11 @@ use Shopware\Core\TestBootstrapper;
 
 $loader = (new TestBootstrapper())
     ->addCallingPlugin()
+    ->addActivePlugins('BlurElysiumSlider')
     ->setForceInstallPlugins(true)
     ->bootstrap()
     ->getClassLoader();
 
-$loader->addPsr4('Blur\\BlurElysiumSlider\\Tests\\Migration\\', __DIR__ . '/Migration');
+// Register plugin namespace (fallback for CI where composer doesn't know about the plugin)
+$loader->addPsr4('Blur\\BlurElysiumSlider\\', __DIR__ . '/../src');
+$loader->addPsr4('Blur\\BlurElysiumSlider\\Tests\\', __DIR__);
