1fb1d95d95485afbede8d73ba73e09b6afa11488
Robert Bisovski <robsenq@gmail.com>
2026-04-10 16:18:52 +0700

    add error indication on cms elysium block

--- src/Resources/app/administration/src/component/cms/blocks/blur-elysium-banner/component/index.ts
+++ src/Resources/app/administration/src/component/cms/blocks/blur-elysium-banner/component/index.ts
@@ -1,8 +1,25 @@
 import type { ComponentConfig } from 'src/core/factory/async-component.factory'
 import template from './template.html.twig';
 
-const { Component } = Shopware;
+const { Component, Store } = Shopware;
 
 export default Component.wrapComponentConfig({
     template,
+    props: ['block'],
+    computed: {
+        blockErrors() {
+            return Store.get('error').getErrorsForEntity(
+                'cms_block',
+                this.block.id
+            );
+        },
+
+        blockClasses() {
+            const classes = {
+                'cms-block-elysium': true,
+                'has-error': Boolean(this.blockErrors)
+            }
+            return classes
+        }
+    },
 }) as ComponentConfig;
